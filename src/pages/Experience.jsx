686009fe59c2bf627bbe54febcb5456cf17ee51a
Anik Roy <anikroy.uiu.ac.bd@gmail.com>
2026-09-17 21:45:13 +0600

    Update Experience.jsx

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,7 +1,14 @@
 import Banner from '../components/Banner';
 
 const experiences = [
-  
+   {
+    role: 'MERN Stack Developer (Lead & Founder)',
+    company: 'Craftbit Tech BD',
+    period: '15 September 2026 - Present',
+    status: 'In Progress',
+    details: "Leading a team of developers to build full-stack web applications using the MERN stack. Responsible for project management, code reviews, and ensuring best practices in development and deployment.",
+    skills: ["React.js", "Node.js", "Express.js", "MongoDB", "JavaScript", "HTML5", "CSS3", "Tailwind CSS", "Bootstrap","Figma", "Git", "GitHub", "RESTful APIs", "JSON", "AJAX", "Web Security Basics"]
+  },
   {
     role: 'Web Developer (Intern)',
     company: 'Creative IT Institute (NSDA level 3 certified)',
